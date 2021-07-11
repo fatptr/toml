000e680f31f653c6df39ed6eccf7e34844552e3f
cristack <mail.fawazabdulla@gmail.com>
2021-07-11 17:04:43 +0530

    ongoing

--- toml.v
+++ toml.v
@@ -1,7 +1,13 @@
 module main
 import toml 
 
+struct Config {
+	name string 
+	age int
+}
+
 fn main() {
-    toml.decode_file("f.toml") 
+	mut conf := Config
+    toml.decode_file("f.toml", &conf) 
 	println('Hello World!')
 }
--- toml/decode.v
+++ toml/decode.v
@@ -1,10 +1,26 @@
 module toml
 import os
+import io
 
-pub fn decode_file(file string){
+struct Decoder {
+	r io.Reader
+}
+
+fn new_decoder(r io.Reader) *Decoder {
+  mut dec := Decoder{r: r}
+  return dec
+}
+
+pub fn (d *Decoder) decode(out interface{})  {
+
+}
+
+
+pub fn decode_file(file string, out interface{}){
 	f := os.open(file) or {
 		println("f")
 		return
 	}
 	println(f)
+	return new_decoder(f).decode(out)
 }
